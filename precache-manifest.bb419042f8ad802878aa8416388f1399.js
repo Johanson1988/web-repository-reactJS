3cccaac2ed0cb1ff80929c0410dd2ccd915c611a
Johann Moreno <johanson88@gmail.com>
2020-05-25 15:45:43 +0200

    Updates

---
+++ precache-manifest.bb419042f8ad802878aa8416388f1399.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8299d834179c3da07780f2a176bd4b71",
+    "url": "/web-repository-reactJS/index.html"
+  },
+  {
+    "revision": "cdd311a19335b4c36d94",
+    "url": "/web-repository-reactJS/static/css/main.464d6b8d.chunk.css"
+  },
+  {
+    "revision": "300a6d642dafcaa2945f",
+    "url": "/web-repository-reactJS/static/js/2.ba8d2f64.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/web-repository-reactJS/static/js/2.ba8d2f64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdd311a19335b4c36d94",
+    "url": "/web-repository-reactJS/static/js/main.95e8d201.chunk.js"
+  },
+  {
+    "revision": "d99d93fb953d455e8692",
+    "url": "/web-repository-reactJS/static/js/runtime-main.67f3c429.js"
+  }
+]);
